c362f1df3b70dd52d96d966cc14c9cdcb548399a
Misode <Misoloo64@gmail.com>
2022-03-23 01:43:41 +0100

    Remove category and seed fields

--- src/fixes/1.19/index.ts
+++ src/fixes/1.19/index.ts
@@ -1,8 +1,14 @@
 import { Fix } from '../../Fix'
+import { Biome } from './Biome'
+import { Dimension } from './Dimension'
 import { Predicates } from './Predicates'
 
 export const Fixes19 = Fix.version('1.18.2', '1.19', Fix.groupProblems(
 	Fix.debug('1.19 fixes'),
+	Fix.when('worldgen',
+		Biome,
+		Dimension,
+	),
 	Fix.when('predicates',
 		Predicates,
 	),
