cc2001ee208acd2274ed9c4fef2705fe21f93bda
Misode <Misoloo64@gmail.com>
2022-04-07 01:47:58 +0200

    22w14a predicate and advancement fixes

--- src/fixes/1.19/Predicates.ts
+++ src/fixes/1.19/Predicates.ts
@@ -39,6 +39,20 @@ function fixCriterion(data: any) {
 	fixLocation(data.conditions.location)
 	fixLocation(data.conditions.entered)
 	fixLocation(data.conditions.exited)
+
+	const trigger = data.trigger.replace(/^minecraft:/, '')
+	switch (trigger) {
+		case 'location':
+		case 'slept_in_bed':
+		case 'hero_of_the_village':
+		case 'voluntary_exile':
+			if (typeof data.conditions !== 'object') break
+			const { player, location } = data.conditions
+			data.conditions = {
+				player: mergePlayerLocation(player, location),
+			}
+			break
+	}
 }
 
 function fixPool(data: any) {
@@ -85,6 +99,36 @@ function fixEntity(data: any) {
 	fixEntity(data.vehicle)
 	fixEntity(data.targeted_entity)
 	fixLocation(data.location)
+
+	if (data.player) {
+		data.type_specific = {
+			type: 'player',
+			player: data.player,
+		}
+		delete data.player
+	} else if (data.fishing_hook) {
+		data.type_specific = {
+			type: 'fishing_hook',
+			...data.fishing_hook,
+		}
+		delete data.fishing_hook
+	} else if (data.lightning_bolt) {
+		data.type_specific = {
+			type: 'lightning',
+			...data.lightning_bolt,
+		}
+		delete data.lightning_bolt
+	} else if (data.catType) {
+		let variant = data.catType.replace(/^textures\/entity\/cat\/([a-z_]+).png/, '$1')
+		if (variant === 'british_shorthair') {
+			variant = 'british'
+		}
+		data.type_specific = {
+			type: 'cat',
+			variant,
+		}
+		delete data.catType
+	}
 }
 
 function fixLocation(data: any) {
@@ -93,5 +137,19 @@ function fixLocation(data: any) {
 	if (typeof data.feature === 'string') {
 		data.structure = data.feature
 		delete data.feature
+	}
+}
+
+function mergePlayerLocation(player: any, location: any) {
+	if (player === undefined) return { location }
+	if (Array.isArray(player) && player.length >= 1) {
+		return [player, { type: 'minecraft:entity_properties', predicate: { location } }]
+	}
+	return {
+		...player,
+		location: {
+			...player.location,
+			...location,
+		},
 	}
 }
