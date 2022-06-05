216549d12acbe5cec66d8ef714a34500127b7fc9
Misode <Misoloo64@gmail.com>
2022-06-05 19:49:44 +0200

    Convert splines and sampling to density functions

--- src/fixes/1.19/NoiseSettings.ts
+++ src/fixes/1.19/NoiseSettings.ts
@@ -17,6 +17,7 @@ export const NoiseSettings = Fix.all(
 	Fix.onFile('worldgen/density_function', ({ name, data }, ctx) => {
 		const id = name.includes(':') ? name : 'minecraft:' + name
 		if (!PROCESSED.has(id)) {
+			console.debug(`Visiting ${id} with default noise`)
 			PROCESSED.add(id)
 			fixDensityFunction(data, ctx, DEFAULT_NOISE)
 		}
@@ -33,11 +34,14 @@ function fixDensityFunction(data: any, ctx: FixContext, noise: any) {
 	if (typeof data === 'string') {
 		const id = data.includes(':') ? data : 'minecraft:' + data
 		if (!PROCESSED.has(id)) {
+			console.debug(`Visiting ${id}`)
 			PROCESSED.add(id)
 			const file = ctx.read('worldgen/density_function', id)
 			if (file && !file.error && !file.deleted) {
 				fixDensityFunction(file.data, ctx, noise)
 			}
+		} else {
+			console.debug(`Already visited ${id}`)
 		}
 		return
 	}
@@ -46,17 +50,91 @@ function fixDensityFunction(data: any, ctx: FixContext, noise: any) {
 
 	const type = data.type.replace(/^minecraft:/, '')
 	switch (type) {
+		case 'abs':
+		case 'blend_density':
+		case 'cache_2d':
+		case 'cache_all_in_cell':
+		case 'cache_once':
+		case 'cube':
+		case 'half_negative':
+		case 'interpolated':
+		case 'quarter_negative':
+		case 'square':
+		case 'squeeze':
+			fixDensityFunction(data.argument, ctx, noise)
+			break
+		case 'add':
+		case 'max':
+		case 'min':
+		case 'mul':
+			fixDensityFunction(data.argument1, ctx, noise)
+			fixDensityFunction(data.argument2, ctx, noise)
+			break
+		case 'clamp':
+		case 'range_choice':
+		case 'weird_scaled_sampler':
+			fixDensityFunction(data.input, ctx, noise)
+			break
+		case 'shifted_noise':
+			fixDensityFunction(data.shift_x, ctx, noise)
+			fixDensityFunction(data.shift_y, ctx, noise)
+			fixDensityFunction(data.shift_z, ctx, noise)
+			break
 		case 'old_blended_noise':
-			// TODO: add sampling fields from noise settings
+			data.xz_scale = noise.sampling.xz_scale
+			data.y_scale = noise.sampling.y_scale
+			data.xz_factor = noise.sampling.xz_factor
+			data.y_factor = noise.sampling.y_factor
 			data.smear_scale_multiplier = 8
 			break
 		case 'spline':
+			fixSpline(data.spline, ctx, noise)
 			delete data.min_value
 			delete data.max_value
 			break
 		case 'terrain_shaper_spline':
 			data.type = 'minecraft:spline'
-			// TODO: add spline
+			fixDensityFunction(data.continentalness, ctx, noise)
+			fixDensityFunction(data.erosion, ctx, noise)
+			fixDensityFunction(data.weirdness, ctx, noise)
+			const coordinates = {
+				continents: data.continentalness,
+				erosion: data.erosion,
+				weirdness: data.weirdness,
+				ridges: 'minecraft:overworld/ridges_folded',
+			}
+			data.spline = fixTerrainSpline(noise.terrain_shaper[data.spline], ctx, coordinates)
 			break
+	}
+}
+
+function fixSpline(data: any, ctx: FixContext, noise: any) {
+	if (typeof data !== 'object') return
+
+	fixDensityFunction(data.coordinate, ctx, noise)
+
+	for (const point of data.points) {
+		fixSpline(point.value, ctx, noise)
+	}
+}
+
+function fixTerrainSpline(data: any, ctx: FixContext, coordinates: Record<string, any>) {
+	if (typeof data !== 'object') {
+		return data
+	}
+
+	let coordinate = coordinates[data.coordinate]
+	if (coordinate === undefined) {
+		ctx.warn(`Unknown coordinate ${data.coordinate}`)
+		coordinate = 0
+	}
+
+	return {
+		coordinate,
+		points: data.points.map((point: any) => ({
+			location: point.location,
+			value: fixTerrainSpline(point.value, ctx, coordinates),
+			derivative: point.derivative,
+		})),
 	}
 }
