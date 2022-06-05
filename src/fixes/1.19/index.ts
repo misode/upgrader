f26cd682acfde1008aecdab7a32459ea8149b823
Misode <Misoloo64@gmail.com>
2022-06-05 22:03:14 +0200

    Actually include the dimension type fix

--- src/fixes/1.19/index.ts
+++ src/fixes/1.19/index.ts
@@ -1,6 +1,7 @@
 import { Fix } from '../../Fix'
 import { Biome } from './Biome'
 import { Dimension } from './Dimension'
+import { DimensionType } from './DimensionType'
 import { Feature } from './Feature'
 import { NoiseSettings } from './NoiseSettings'
 import { Predicates } from './Predicates'
@@ -11,6 +12,7 @@ export const Fixes19 = Fix.version('1.18.2', '1.19', Fix.groupProblems(
 	Fix.when('worldgen',
 		Biome,
 		Dimension,
+		DimensionType,
 		NoiseSettings,
 		Structure,
 		Feature,
