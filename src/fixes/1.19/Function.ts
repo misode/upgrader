96bbbb0d355e51085b759aa50ba1d3d559b7f947
Misode <Misoloo64@gmail.com>
2022-06-22 20:21:12 +0200

    Fix effect ID in functions

--- src/fixes/1.19/Function.ts
+++ src/fixes/1.19/Function.ts
@@ -1,5 +1,8 @@
 import { Fix } from '../../Fix'
 
+const EffectPattern = /\b((?:CustomPotionEffects|ActiveEffects|Effects):.+\bId\s*:\s*\d+)b\b/
+const BeaconEffectPattern = /\b((?:Primary|Secondary)\s*:\s*\d+)b\b/
+
 export const Function = Fix.onFile('functions', ({ data }: { data: string[] }) => {
 	data.forEach((line, i) => {
 		if (line.startsWith('locate ') || line.startsWith('execute ')) {
@@ -10,6 +13,12 @@ export const Function = Fix.onFile('functions', ({ data }: { data: string[] }) =
 		}
 		if (line.startsWith('placefeature ') || line.startsWith('execute ')) {
 			data[i] = data[i].replace('placefeature ', 'place feature ')
+		}
+		if (line.includes('Id')) {
+			data[i] = data[i].replace(EffectPattern, '$1')
+		}
+		if (line.includes('Primary') || line.includes('Secondary')) {
+			data[i] = data[i].replace(BeaconEffectPattern, '$1')
 		}
 	})
 })
